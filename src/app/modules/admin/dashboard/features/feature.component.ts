685fb3d9f12153c0b449508df32a05963e3a52e7
datiscity <dat@iscity.com.my>
2022-03-10 18:07:45 +0700

    ADD background icon for card in unique feature

--- src/app/modules/admin/dashboard/features/feature.component.ts
+++ src/app/modules/admin/dashboard/features/feature.component.ts
@@ -10,12 +10,30 @@ import { Component, OnInit, OnDestroy , Input} from '@angular/core';
     templateUrl: './feature.component.html',
 })
 export class FeatureComponent implements OnInit {
-
+    isactive1 = true;
+    isactive2 = false;
+    isactive3 = false;
     constructor(
     ) {}
     
     
     ngOnInit() {
         
+    }
+
+    changetab(tab : number){
+        if (tab == 1) {
+            this.isactive1 = true;
+            this.isactive2 = false;
+            this.isactive3 = false;
+        }else if(tab == 2){
+            this.isactive2 = true;
+            this.isactive1 = false;
+            this.isactive3 = false;
+        } else {
+            this.isactive3 = true;
+            this.isactive1 = false;
+            this.isactive2 = false;
+        }
     }
 }
